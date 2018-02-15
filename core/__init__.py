faaccef7b106a7c30f59d4f84e50445736907d2b
Romain Feron <romain.feron@evobio.eu>
2018-02-15 15:59:13 +0100

    Implemented opening fasta with multiple sequences

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,3 @@
 from .traduction import dna_to_rna, rna_to_dna, dna_to_protein
-from .seq_utils import reverse_complement, open_fasta
+from .seq_utils import reverse_complement, open_fasta, open_fasta_multiple
 from .kmers import ordered_kmers
--- core/seq_utils.py
+++ core/seq_utils.py
@@ -2,10 +2,22 @@ from .resources import complement
 
 
 def reverse_complement(sequence):
-    return(''.join(complement[base] for base in reversed(sequence)))
+    return ''.join(complement[base] for base in reversed(sequence))
 
 
 def open_fasta(input_file):
 
     input_file.readline()
-    return(''.join(line.rstrip('\n') for line in input_file))
+    return ''.join(line.rstrip('\n') for line in input_file)
+
+
+def open_fasta_multiple(input_file):
+
+    output = {}
+    temp = ''
+    for line in input_file:
+        if line.startswith('>'):
+            temp = line[1:-1]
+        else:
+            output[temp] = line[:-1]
+    return output
